601febc187f556f804e886893c7511403a9391bd
Vaibhav Kadu <vkdon02@gmail.com>
2026-03-04 18:54:44 +0530

    Fresh Run Tested

---
+++ src/main/java/com/vkmaster/hotelmanagement/HotelManagementApplication.java
@@ -0,0 +1,14 @@
+package com.vkmaster.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementApplication.class, args);
+        System.out.println("=====================***************************   vk  =====================***************************");
+    }
+
+}
